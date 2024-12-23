aa6128848fb128b47f4ade7a1d22fcf685f392d9
Vlad Markuts <vlad.markuts@gmail.com>
2024-12-23 18:07:06 +0200

    Update IERC20.sol

--- IERC20.sol
+++ IERC20.sol
@@ -1,10 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-/**
- * @title IERC20
- * @dev Интерфейс стандарта ERC20, определяющий основные функции токена.
- */
 interface IERC20 {
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
